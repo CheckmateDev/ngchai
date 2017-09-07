d703fb631f94c5bf4a99bf326eefa5a1180021a8
Michael Solati <mkslt04@gmail.com>
2017-09-07 16:16:38 -0400

    build(tag): set up build script to set cron builds to be nightly version

--- cron-nightly.js
+++ cron-nightly.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 exec(`git show -s --format=%ct \$(git rev-parse --verify --short HEAD)`, (error, epoch) => {
   if (!error) {
     const time = new Date(epoch * 1000).toISOString().split('T')[0].replace(/\W/g, '');
-    package.version = (package.version + '-dev.' + time).replace(/\r?\n|\r/g, '');
+    package.version = (package.version + '-nightly.' + time).replace(/\r?\n|\r/g, '');
     fs.writeFile('./dist/package.json', JSON.stringify(package), 'utf8', (error) => {
       if (error) {
         throw error;
